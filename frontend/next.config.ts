642a59bd19a2f3c7ef979bfe44998e6cddffdbf9
Natan <natantes36@gmail.com>
2026-07-07 12:25:34 +0300

    Stop forcing frontend www redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,17 +11,6 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "yehagerbahillibs.com",
-          },
-        ],
-        destination: "https://www.yehagerbahillibs.com/:path*",
-        permanent: true,
-      },
-      {
         source: "/islamic",
         destination: "/catalog",
         permanent: true,
